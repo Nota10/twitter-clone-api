5e305fc6ebb1f02c1d5a9a3b281367158d21f300
Nicolas Bortoluzzi <bortoluzzinicolas@gmail.com>
2021-11-30 20:38:10 -0300

    Remove aws

--- src/main-aws.ts
+++
@@ -1,7 +0,0 @@
-import { lambda } from '@gcoreplus/nestjs-aws-serverless';
-import { AppModule } from './app.module';
-
-export const handler = lambda(AppModule, {
-  engine: 'express',
-  nestjs: { options: { cors: true } },
-});
