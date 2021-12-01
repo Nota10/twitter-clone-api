52ab40192b0accac164456a535a09e0d1e371565
Nicolas Bortoluzzi <bortoluzzinicolas@gmail.com>
2021-12-01 10:42:34 -0300

    Add new tweet methods

--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -1,6 +1,6 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { CreateResponse } from '../common/responses/create.response';
 import { DeleteResponse } from '../common/responses/delete.response';
 import { FindResponse } from '../common/responses/find.response';
@@ -19,7 +19,7 @@ export class TweetService {
   async create(createTweetDto: CreateTweetDto, user: any): Promise<CreateResponse<Tweet>> {
     if (!user) {
       return {
-        status: HttpStatus.NON_AUTHORITATIVE_INFORMATION,
+        status: HttpStatus.UNAUTHORIZED,
         message: "User not found",
         error: "Unauthorized"
       }
@@ -49,26 +49,45 @@ export class TweetService {
       var hashTags, i, len, word, words;
       words = text.split(/[\s\r\n]+/);
       hashTags = [];
+
       for (i = 0, len = words.length; i < len; i++) {
         word = words[i];
         if (word.indexOf('#') === 0) {
           hashTags.push(word.replace('#', ''));
         }
       }
+
       return hashTags;
     }
   }
 
-  async findAll(user: any): Promise<FindResponse<Tweet>> {
+  async findAll(user: any, params: any): Promise<FindResponse<Tweet>> {
     try {
-      let query = {};
-      if (user !== undefined) {
-        query = {"user.id": user._id};
+      if (user === undefined) {
+        return {
+          status: HttpStatus.UNAUTHORIZED,
+          message: 'The user is not logged',
+          error: 'Unauthorized',
+        };
       }
 
-      console.log(query)
+      const query = [];
+      query.push({ 
+        $match: {"user.id": user._id}
+      });
 
-      const tweets = await this.tweetModel.find(query, '-__v').exec();
+      if (params.get_childs === 'true') {
+        query.push({
+          $lookup: {
+            from: 'tweets',
+            localField: '_id',
+            foreignField: 'parentTweetId',
+            as: 'childList'
+          }
+        });
+      }
+
+      const tweets = await this.tweetModel.aggregate(query).exec()
 
       if (tweets.length === 0) {
         return {
@@ -83,7 +102,7 @@ export class TweetService {
         message: 'Tweets found',
         data: tweets
       };
-    } catch (error) {
+    } catch (error: any) {
       return {
         status: HttpStatus.BAD_REQUEST,
         message: error.message,
@@ -92,9 +111,9 @@ export class TweetService {
     }
   }
 
-  async findOne(id: string): Promise<FindIdResponse<Tweet>> {
+  async findOne(id: string, params: any): Promise<FindIdResponse<object>> {
     try {
-      const tweet = await this.tweetModel.findOne({_id: id});
+      const tweet = await this.tweetModel.findOne({_id: id}, '-__v');
 
       if (!tweet) {
         return {
@@ -104,12 +123,18 @@ export class TweetService {
         };
       }
 
+      tweet.set('childList', [], {strict: false});
+      if (params.get_childs === 'true') {
+        const childTweets = await this.tweetModel.find({parentTweetId: tweet._id}, '-__v');
+        tweet.set('childList', childTweets, {strict: false});
+      }
+
       return {
         status: HttpStatus.OK,
         message: 'Tweet found',
         data: tweet,
       };    
-    } catch (error) {
+    } catch (error: any) {
       return {
         status: HttpStatus.BAD_REQUEST,
         message: error.message,
@@ -143,5 +168,102 @@ export class TweetService {
       status: HttpStatus.OK,
       message: 'Tweet deleted'
     };   
+  }
+
+  async like(id: string, user: any): Promise<FindIdResponse<Tweet>> {
+    const tweet = await this.tweetModel.findOne({_id: id});
+
+    if (!tweet) {
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: `Tweet not found with the given id ${id}`,
+        error: 'Not Found',
+      };
+    }
+
+    const userIdIndex = tweet.likeList.indexOf(user._id);
+    if (userIdIndex === -1) {
+      tweet.likeCount += 1;
+      tweet.likeList.push(user._id);
+    } else {
+      tweet.likeCount -= 1;
+      tweet.likeList.splice(userIdIndex, 1);
+    }
+
+    await tweet.save();
+
+    return {
+      status: HttpStatus.OK,
+      message: 'Tweet found',
+      data: tweet,
+    };   
+  }
+
+  async deslike(id: string, user: any): Promise<FindIdResponse<Tweet>> {
+    const tweet = await this.tweetModel.findOne({_id: id});
+
+    if (!tweet) {
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: `Tweet not found with the given id ${id}`,
+        error: 'Not Found',
+      };
+    }
+
+    const userIdIndex = tweet.deslikeList.indexOf(user._id);
+    if (userIdIndex === -1) {
+      tweet.deslikeCount += 1;
+      tweet.deslikeList.push(user._id);
+    } else {
+      tweet.deslikeCount -= 1;
+      tweet.deslikeList.splice(userIdIndex, 1);
+    }
+
+    await tweet.save();
+
+    return {
+      status: HttpStatus.OK,
+      message: 'Tweet found',
+      data: tweet,
+    };   
+  }
+
+  async share(id: string, user: any): Promise<FindIdResponse<Tweet>> {
+    const parentTweet = await this.tweetModel.findOne({_id: id});
+
+    if (!parentTweet) {
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: `Tweet not found with the given id ${id}`,
+        error: 'Not Found',
+      };  
+    }
+
+    const userShort = new this.userShortModel({
+      id: user._id,
+      name: user.name ,
+      username: user.username,
+      protected: user.protected,
+      avatar: user.avatar
+    })
+
+    const childTweet = new this.tweetModel(parentTweet);
+
+    childTweet._id = new Types.ObjectId();
+    childTweet.parentTweetId = parentTweet._id;
+    childTweet.user = userShort;
+    childTweet.deslikeCount = 0;
+    childTweet.deslikeList = [];
+    childTweet.likeCount = 0;
+    childTweet.likeList = [];
+    childTweet.isNew = true;
+
+    await childTweet.save();
+
+    return {
+      status: HttpStatus.OK,
+      message: `Tweet shared`,
+      data: childTweet,
+    };  
   }
 }
